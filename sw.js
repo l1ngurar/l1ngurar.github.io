9222d4c775e3628ff4594779bcc6c8a6b43e0331
Maxim Lingurar <90052713+l1ngurar@users.noreply.github.com>
2026-05-20 16:37:27 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'skladrasklad-v3';
+const urlsToCache = [
+  '.',
+  'index.html',
+  'manifest.json',
+  'icon-64.png',
+  'icon-128.png',
+  'icon-256.png',
+  'icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // Обновляем кэш свежей версией
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => key !== CACHE_NAME && caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
